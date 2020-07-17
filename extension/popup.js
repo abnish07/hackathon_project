52452b49f41d53c8f1aa234d3b3bf41afdd584a4
Abnish <akp12556@gmail.com>
2020-07-18 03:50:37 +0530

    notifications updates

--- extension/popup.js
+++ extension/popup.js
@@ -3,11 +3,43 @@
 const searchText = document.getElementById("search");
 const header = document.getElementById('header');
 const btn = document.getElementById('btn1');
+const trackerBtn = document.getElementById('counterBtn')
+// const hello = document.querySelector('h1')
+
 var websites = []
+var count = 0;
+
+trackerBtn.addEventListener('click',()=>{
+  console.log(count)
+
+  count +=1
+  
+  var timer = setInterval(()=>{
+    count++
+    hello.textContent += count;
+    if(count ==5){
+      var notifiOptions = {
+        type: "progress",
+        iconUrl: "images/logot48.png",
+        title: "Time Remainder",
+        message:"Hey! you have already spent 30 min on this website" 
+      }
+      chrome.notifications.create('nitifi', notifiOptions, ()=>{
+        console.log(count)
+      })
+    }
+        if(count == 10){
+          clearTimeout(timer);
+          chrome.notifications.clear("nitifi", notifiOptions)
+        }
+  }, 1000)
+  
+})
 
 
 btn.addEventListener('click', ()=>{
   var text = searchText.value;
+  
   websites.push(text)
   console.log(websites)
   localStorage.setItem("data", JSON.stringify(websites || null))
@@ -21,6 +53,7 @@ btn.addEventListener('click', ()=>{
   header.append(p)
   searchText.value = ""
 });
+
 
 
 // const replaceText = document.getElementById("replace");
